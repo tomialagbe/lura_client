07df10252895c8cf5d4535075ec204b20d2221fb
Tomi <oalagbe@TLL-C02DRBEMMD6R.local>
2022-03-22 23:44:17 +0200

    Update app icons

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import bonsoir
 import firebase_auth
 import firebase_core
 import shared_preferences_macos
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BonsoirPlugin.register(with: registry.registrar(forPlugin: "BonsoirPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
