78bd2a2e8c99e0db5f0445ff1c8d69a39da8eeb2
Tomi <oalagbe@TLL-C02DRBEMMD6R.lan>
2022-04-04 18:19:15 +0300

    Fix main_prod server URL

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -4,7 +4,7 @@ import 'firebase_options_prod.dart';
 import 'main.dart' as m;
 
 final _config = LuraConfig(
-    serverUrl: 'https://app.lura.so',
+    serverUrl: 'https://api.lura.so',
     firebaseOptions: ProdFirebaseOptions.currentPlatform);
 
 void main() => m.main(config: _config);
